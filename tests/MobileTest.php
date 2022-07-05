287c42be909f51b9a77e3db67d115f2f3f491efa
Alexis <ale.maldo097@gmail.com>
2022-07-04 20:52:23 -0500

    Fix first Test

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -2,18 +2,27 @@
 
 namespace Tests;
 
+use App\Interfaces\CarrierInterface;
+use App\Mobile;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class MobileTest extends TestCase
 {
-	
-	/** @test */
-	public function it_returns_null_when_name_empty()
-	{
-		$mobile = new Mobile($provider);
+    private $provider;
 
-		$this->assertNull($mobile->makeCallByName(''));
-	}
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->provider = m::mock(CarrierInterface::class);
+    }
+
+    /** @test */
+    public function it_returns_null_when_name_empty()
+    {
+        $mobile = new Mobile($this->provider);
+
+        $this->assertNull($mobile->makeCallByName(''));
+    }
 
 }
